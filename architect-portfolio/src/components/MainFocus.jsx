0e29112f61cf2b096772c8b7df05ed32823ecdec
Melina Schild <melina.schild@icloud.com>
2023-10-30 12:06:41 +0100

    main focus section

--- architect-portfolio/src/components/MainFocus.jsx
+++ architect-portfolio/src/components/MainFocus.jsx
@@ -1,5 +1,26 @@
 const MainFocus = () => {
-  return <section></section>;
+  return (
+    <section className="main-focus">
+      <h2>Main Focus/Mission Statement</h2>
+      <article>
+        <div>
+          <p className="number one">1</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            efficitur, lectus et facilisis placerat.
+          </p>
+        </div>
+        <div>
+          <p className="number">2</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            efficitur, lectus et facilisis placerat, magna mauris porttitor
+            tortor, a auctor est felis ut nisl.
+          </p>
+        </div>
+      </article>
+    </section>
+  );
 };
 
 export default MainFocus;
